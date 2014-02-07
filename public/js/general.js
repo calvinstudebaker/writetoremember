4d9dd262cd02eca50dae354a9a1f03e79fd141ea
Calvin Studebaker <calstud@stanford.edu>
2014-02-06 22:13:49 -0800

    created sign in page

--- public/js/general.js
+++ public/js/general.js
@@ -11,4 +11,17 @@ $(document).ready(function() {
 function initializePage() {
 	$(".container").hide();
 	$(".container").show("slow");
+
+	$("input[type=text], input[type=password]").focusin(function() {
+		$(this).removeClass("error").addClass("highlighted");
+	}).focusout(function() {
+		$(this).removeClass("highlighted").removeClass("error");
+	});
 }
+
+function switchToPage(page){
+	$(".content").hide("fast");
+	setTimeout(function() {
+		$(location).attr('href', page);
+	}, 300);
+};
---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+function signIn(){
+	var email = $("#email").text();
+	var password = $("#password").text();
+	if(true){	//will be a valid email password check soon
+		switchToPage("/home");
+	}
+}
