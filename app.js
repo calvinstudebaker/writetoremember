32d228ebfab18b6f4b3d62a020757437edc30305
Calvin Studebaker <calstud@stanford.edu>
2014-02-06 23:00:58 -0800

    adding homepage

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
 
-var index = require('./routes/index');
+var index = require('./routes/indexRoute');
+var home = require('./routes/homeRoute');
 
 var app = express();
 
@@ -34,6 +35,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/home', home.view);
 // Example route
 // app.get('/users', user.list);
 
---
+++ public/js/home.js
@@ -0,0 +1,23 @@
+//adapted from: https://facultystaff.richmond.edu/~creamer/web/js/date.html
+function getDate(){
+	// Array of day names
+	var dayNames = new Array("Sunday","Monday","Tuesday","Wednesday",
+					"Thursday","Friday","Saturday");
+
+	// Array of month Names
+	var monthNames = new Array(
+	"January","February","March","April","May","June","July",
+	"August","September","October","November","December");
+
+	var now = new Date();
+	var date = dayNames[now.getDay()] + ", " + 
+	monthNames[now.getMonth()] + " " + 
+	now.getDate() + ", " + now.getFullYear();
+
+	$("#dateLocation").append(date);
+	
+}
+
+$(document).ready(function() {
+	getDate();
+})
