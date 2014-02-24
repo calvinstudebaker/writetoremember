23451bf78497b01f4794b8fbfa8d0e05765501a8
Mikaela Grace <mgrace@stanford.edu>
2014-02-23 21:57:32 -0800

    small changes to home

--- public/js/home.js
+++ public/js/home.js
@@ -56,17 +56,15 @@ function uploadMedia(){
 	}
 }
 
-<<<<<<< HEAD
 function getRandomEntry() {
 	$.post("/getRandomEntry", data, function(response){
 		$('#randomPlaceholder').html("TEST STUFF")
 }
-=======
+
 function showEntryError() {
 	$('#errorPlaceholder').html("Please enter some text!"); 
 }
 
->>>>>>> 731df53aa5ad7e6a1e49877116253650a2c07bd9
 $(document).ready(function() {
 	getDate();
 	$("#pastButton").click(function(){
